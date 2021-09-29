432dca07dde42d55d923a71fe5fa59fea3778190
Jhon Gutiérrez <jhongutierrez7011@gmail.com>
2021-09-29 17:14:49 -0500

    Docs agregado

---
+++ Docs/precache-manifest.d376dccd9a2c4d2597fe77bde8a159a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d3b38287762ec0049619141004ab60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e98916ed246a532f44d6",
+    "url": "/static/css/main.21ffb527.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98916ed246a532f44d6",
+    "url": "/static/js/main.65e6d4ec.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
